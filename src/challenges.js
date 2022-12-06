05525a41d30a9f224edf948f82a41b58c124d1dc
Weslley Moura <schno@WeslleyMoura.localdomain>
2022-12-06 15:27:55 -0300

    Finaliza desafio 6

--- src/challenges.js
+++ src/challenges.js
@@ -35,6 +35,24 @@ function highestCount(array) {
 }
 
 // Desafio 6 - Crie as funções calcTriangleArea, calcRectangleArea e calcAllAreas
+const calcTriangleArea = (base, height) => base * height / 2;
+const calcRectangleArea = (base, height) => base * height;
+
+const calcAllAreas = (base, height, form) => {
+  if (form === 'triângulo'){
+    return ('O valor da área do triângulo é de: ' + (calcTriangleArea(base, height)))
+  }
+  else if (form === 'retângulo'){
+    return ('O valor da área do retângulo é de: ' + (calcRectangleArea(base, height)))
+  }
+  else {
+    return ('Não foi possível fazer o cálculo, insira uma forma geométrica válida')
+  }
+}
+
+
+// console.log(`A área do triângulo é: ${calcAllAreas(base, height, form)}`);
+
 
 // Desafio 7 - Crie a função catAndMouse
 
